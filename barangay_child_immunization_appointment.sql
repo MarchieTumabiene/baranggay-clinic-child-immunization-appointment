b86c3b2139c71b8247c0b189000910edb264ce87
MJ Ofianga <maritchetumabiene@gmail.com>
2024-07-05 15:47:45 +0800

    first commit

--- barangay_child_immunization_appointment.sql
+++ barangay_child_immunization_appointment.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 30, 2024 at 10:46 PM
+-- Generation Time: Jul 05, 2024 at 09:47 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -50,16 +50,64 @@ INSERT INTO `admin` (`id`, `username`, `password`, `created_at`, `updated_at`) V
 
 CREATE TABLE `appointments` (
   `id` int(11) NOT NULL,
-  `p_fname` text NOT NULL,
-  `p_mname` text DEFAULT NULL,
-  `p_lname` text NOT NULL,
+  `barangay` text NOT NULL,
+  `child_no` varchar(100) NOT NULL,
   `c_fname` text NOT NULL,
   `c_mname` text DEFAULT NULL,
   `c_lname` text NOT NULL,
-  `date` date NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `date_seen` date NOT NULL,
+  `date_birth` date NOT NULL,
+  `birth_weight` float NOT NULL,
+  `place_delivery` text NOT NULL,
+  `birth_registered` text NOT NULL,
+  `address` text NOT NULL,
   `email` varchar(255) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT 1,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `appoint_parents`
+--
+
+CREATE TABLE `appoint_parents` (
+  `id` int(11) NOT NULL,
+  `appoint_id` int(11) DEFAULT NULL,
+  `m_fname` text DEFAULT NULL,
+  `m_mname` text DEFAULT NULL,
+  `m_lname` text DEFAULT NULL,
+  `m_education` text DEFAULT NULL,
+  `m_occupation` text DEFAULT NULL,
+  `f_fname` text DEFAULT NULL,
+  `f_mname` text DEFAULT NULL,
+  `f_lname` text DEFAULT NULL,
+  `f_education` text DEFAULT NULL,
+  `f_occupation` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `immunization`
+--
+
+CREATE TABLE `immunization` (
+  `id` int(11) NOT NULL,
+  `appoint_id` int(11) DEFAULT NULL,
+  `newborn_screening` varchar(100) DEFAULT NULL,
+  `BCG` varchar(100) DEFAULT NULL,
+  `DRT` varchar(100) DEFAULT NULL,
+  `CRV` varchar(100) DEFAULT NULL,
+  `HEPATITIS_B` varchar(100) DEFAULT NULL,
+  `MEASLES` varchar(100) DEFAULT NULL,
+  `VITAMIN_A` varchar(100) DEFAULT NULL,
+  `VITAMIN_K` varchar(100) DEFAULT NULL,
+  `DEWORMING` varchar(100) DEFAULT NULL,
+  `DENTAL_CHECK_UP` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -79,6 +127,18 @@ ALTER TABLE `appointments`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `appoint_parents`
+--
+ALTER TABLE `appoint_parents`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `immunization`
+--
+ALTER TABLE `immunization`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -92,6 +152,18 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `appoint_parents`
+--
+ALTER TABLE `appoint_parents`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `immunization`
+--
+ALTER TABLE `immunization`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 

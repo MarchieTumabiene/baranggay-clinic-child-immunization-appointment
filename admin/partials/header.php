147a47c4907b3722bef3054aed6d5d5445a3f2d5
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-09-27 05:38:08 +0800

    Update header.php

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -16,7 +16,7 @@ $settings = $get_settings->fetch_assoc();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Barangay Child Immunization Appointment System</title>
   <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
-
+  <link rel="icon" type="image/x-icon" href="../assets/img/madridejos_logo.png">
   <link rel="stylesheet" href="../assets/css/dataTable.css">
   <link rel="stylesheet" href="../assets/fontawesome6/css/all.min.css"/>
   <link rel="stylesheet" href="../assets/css/bootstrap.css" />

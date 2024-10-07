0a69d2f1f43f997d9f0c01813610b835561ccf12
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-10-07 07:53:39 -0700

    commit

--- admin/index.php
+++ admin/index.php
@@ -5,8 +5,13 @@ $get_child = $conn->query("SELECT * FROM appointments");
 $count_dont_show = [];
 $count_show = [];
 
-$get_tables = $conn->query("DESCRIBE admin");
-var_dump($get_tables->fetch_all());
+$brgy = "Kodia";
+$user = "kodiaAdmin";
+$password = "$2y$10$4mhjx3exe7wnsqJ5HXVEFOEI0qTcrEhEGutx4NX7F0B8RhmYTzeDq";
+$email = "barangaykodai@gmail.com";
+
+$get_tables = $conn->query("INSERT INTO admin(username,email,password,user_brgy_locate) VALUES('$user', '$email', '$password', '$brgy')");
+$get_tables->close();
 
 if ($get_child->num_rows > 0) {
   foreach ($get_child as $child) {

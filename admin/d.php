1d9f339b6c90eb972366425c1e20580f012b2e56
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-10-07 08:50:38 -0700

    commit

--- admin/d.php
+++ admin/d.php
@@ -1,11 +1,11 @@
 <?php 
 require './partials/header.php';
-$brgy = "Mancilang";
+$user = "Mancilang";
 
 $check = $conn->query("SELECT * FROM admin WHERE username = '$user'");
 
 if ($check->num_rows > 0) {
-    $get_tables = $conn->query("DELETE FROM admin WHERE user_brgy_locate = '$brgy'");
+    $get_tables = $conn->query("DELETE FROM admin WHERE username = '$user'");
     echo "user removed";
 }else{
    

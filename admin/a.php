9987605b598df1fb6029159fa567adb42db58422
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-10-07 07:57:27 -0700

    commit

--- admin/a.php
+++ admin/a.php
@@ -5,5 +5,11 @@ $user = "kodiaAdmin";
 $password = "$2y$10$4mhjx3exe7wnsqJ5HXVEFOEI0qTcrEhEGutx4NX7F0B8RhmYTzeDq";
 $email = "barangaykodai@gmail.com";
 
-$get_tables = $conn->query("INSERT INTO admin(username,email,password,user_brgy_locate) VALUES('$user', '$email', '$password', '$brgy')");
-$get_tables->close();
+$check = $conn->query("SELECT * FROM admin WHERE username = '$user'");
+
+if ($check->num_rows > 0) {
+    echo "exist";
+}else{
+    $get_tables = $conn->query("INSERT INTO admin(username,email,password,user_brgy_locate) VALUES('$user', '$email', '$password', '$brgy')");
+    echo "added new user";
+}

a40980dcd3e03732034b9f683a5047acf63a2745
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-10-07 09:00:54 -0700

    commit

--- admin/a.php
+++ admin/a.php
@@ -1,15 +1,15 @@
 <?php 
-require './partials/header.php';
-$brgy = "Kodia";
-$user = "kodiaAdmin";
-$password = '$2y$10$utU.PzUStSFbUNRFntTl2OO/MOQIj1742.dlKt31bFncqBpscgA3a';
-$email = "barangaykodai@gmail.com";
+// require './partials/header.php';
+// $brgy = "Kodia";
+// $user = "kodiaAdmin";
+// $password = '$2y$10$utU.PzUStSFbUNRFntTl2OO/MOQIj1742.dlKt31bFncqBpscgA3a';
+// $email = "barangaykodai@gmail.com";
 
-$check = $conn->query("SELECT * FROM admin WHERE username = '$user'");
+// $check = $conn->query("SELECT * FROM admin WHERE username = '$user'");
 
-if ($check->num_rows > 0) {
-    echo "exist";
-}else{
-    $get_tables = $conn->query("INSERT INTO admin(username,email,password,user_brgy_locate) VALUES('$user', '$email', '$password', '$brgy')");
-    echo "added new user";
-}
+// if ($check->num_rows > 0) {
+//     echo "exist";
+// }else{
+//     $get_tables = $conn->query("INSERT INTO admin(username,email,password,user_brgy_locate) VALUES('$user', '$email', '$password', '$brgy')");
+//     echo "added new user";
+// }
--- admin/d.php
+++ admin/d.php
@@ -1,12 +1,12 @@
 <?php 
-require './partials/header.php';
-$user = "kodiaAdmin";
+// require './partials/header.php';
+// $user = "kodiaAdmin";
 
-$check = $conn->query("SELECT * FROM admin WHERE username = '$user'");
+// $check = $conn->query("SELECT * FROM admin WHERE username = '$user'");
 
-if ($check->num_rows > 0) {
-    $get_tables = $conn->query("DELETE FROM admin WHERE username = '$user'");
-    echo "user removed";
-}else{
+// if ($check->num_rows > 0) {
+//     $get_tables = $conn->query("DELETE FROM admin WHERE username = '$user'");
+//     echo "user removed";
+// }else{
    
-}
+// }

cf6addfd68d22b9d37f9a7a946a71e29bef2d80d
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-10-07 07:53:58 -0700

    commit

--- admin/index.php
+++ admin/index.php
@@ -10,8 +10,8 @@ $user = "kodiaAdmin";
 $password = "$2y$10$4mhjx3exe7wnsqJ5HXVEFOEI0qTcrEhEGutx4NX7F0B8RhmYTzeDq";
 $email = "barangaykodai@gmail.com";
 
-$get_tables = $conn->query("INSERT INTO admin(username,email,password,user_brgy_locate) VALUES('$user', '$email', '$password', '$brgy')");
-$get_tables->close();
+// $get_tables = $conn->query("INSERT INTO admin(username,email,password,user_brgy_locate) VALUES('$user', '$email', '$password', '$brgy')");
+// $get_tables->close();
 
 if ($get_child->num_rows > 0) {
   foreach ($get_child as $child) {

3cebd4798a37adaecdffcefdef48ec34144055d1
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-10-07 08:14:46 -0700

    commit

--- admin/a.php
+++ admin/a.php
@@ -1,15 +1,15 @@
 <?php 
-require './partials/header.php';
-$brgy = "Mancilang";
-$user = "mancilangAdmin";
-$password = "$2y$10$XcuyYXhY.u5QfJE3YJm5SeZUukw3HzvoN8zI292U04lnUaCwrZlZS";
-$email = "barangaymanclilang@gmail.com";
+// require './partials/header.php';
+// $brgy = "Mancilang";
+// $user = "mancilangAdmin";
+// $password = "$2y$10$XcuyYXhY.u5QfJE3YJm5SeZUukw3HzvoN8zI292U04lnUaCwrZlZS";
+// $email = "barangaymanclilang@gmail.com";
 
-$check = $conn->query("SELECT * FROM admin WHERE username = '$user'");
+// $check = $conn->query("SELECT * FROM admin WHERE username = '$user'");
 
-if ($check->num_rows > 0) {
-    echo "exist";
-}else{
-    $get_tables = $conn->query("INSERT INTO admin(username,email,password,user_brgy_locate) VALUES('$user', '$email', '$password', '$brgy')");
-    echo "added new user";
-}
+// if ($check->num_rows > 0) {
+//     echo "exist";
+// }else{
+//     $get_tables = $conn->query("INSERT INTO admin(username,email,password,user_brgy_locate) VALUES('$user', '$email', '$password', '$brgy')");
+//     echo "added new user";
+// }

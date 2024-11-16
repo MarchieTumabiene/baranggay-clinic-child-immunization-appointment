454455cfbf949e16cd530dbcd14a9f48c15d9e24
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-11-17 01:36:55 +0800

    commit

--- admin/admins.php
+++ admin/admins.php
@@ -1,32 +1,6 @@
 <?php
 require './partials/header.php';
-$get_appointments = $conn->query("SELECT 
-        a.*, 
-        CONCAT(p.m_fname, ' ', p.m_mname, ' ', p.m_lname) AS mother, 
-        CONCAT(p.f_fname, ' ', p.f_mname, ' ', p.f_lname) AS father, 
-        CONCAT(c_fname, ' ', c_mname, ' ', c_lname) AS child
-    FROM 
-    appointments a 
-    INNER JOIN 
-        appoint_parents p ON a.id = p.appoint_id
-    WHERE 
-        a.status = 1 AND a.barangay = '$barangay'
-    ");
-
-    if ($barangay == 'admin') {
-        $get_appointments = $conn->query("SELECT 
-        a.*, 
-        CONCAT(p.m_fname, ' ', p.m_mname, ' ', p.m_lname) AS mother, 
-        CONCAT(p.f_fname, ' ', p.f_mname, ' ', p.f_lname) AS father, 
-        CONCAT(c_fname, ' ', c_mname, ' ', c_lname) AS child
-    FROM 
-    appointments a 
-    INNER JOIN 
-        appoint_parents p ON a.id = p.appoint_id
-    WHERE 
-        a.status = 1
-    ");
-    }
+$get_barangayAdmins = $conn->query("SELECT * FROM admin");
 ?>
 <div class="container-fluid">
 
@@ -41,41 +15,33 @@ $get_appointments = $conn->query("SELECT
                 <div class="card">
                     <div class="card-header d-flex align-items-center gap-2">
                         <h5 class="mb-0"><i class="fa fa-calendar-check"></i> Appointments</h5>
-                       <?php 
-                        if ($barangay != 'admin') {
-                         ?>
+                      
                           <a href="./create-appointment.php" class="btn btn-primary"><i class="fa fa-plus"></i></a>
-                         <?php    
-                        }
-                       ?>
+                        
                     </div>
                     <div class="card-body table-responsive">
 
                         <table id="table">
                             <thead>
                                 <th>#</th>
-                                <th>Ref. #</th>
-                                <th>Mother</th>
-                                <th>Father</th>
-                                <th>Child</th>
+                                <th>User Name</th>
+                                <th>Email</th>
                                 <th>Barangay</th>
-                                <th>Date</th>
                                 <th>Action</th>
                             </thead>
 
                             <tbody>
                                 <?php
                                 $i = 1;
-                                foreach ($get_appointments as $row) :
+                                foreach ($get_barangayAdmins as $row) :
                                 ?>
                                     <tr>
                                         <td><?= $i++ ?></td>
-                                        <td><?= $row['reference_id'] ?? '' ?></td>
-                                        <td><?= $row['mother'] ?></td>
+                                        <td><?= $row['username'] ?? '' ?></td>
+                                        <td><?= $row['email'] ?></td>
                                         <td><?= $row['father'] ?></td>
-                                        <td><?= $row['child'] ?></td>
                                         <td><?= $row['barangay'] ?></td>
-                                        <td><?= $row['date_appoint'] ?></td>
+                                       
                                         <td class="d-flex align-items-center gap-2">
                                             <a href="#"  onclick="showMessage('Are you sure you want to delete this appointment?', 'question', '?action=delete-appointment&id=<?= $row['id'] ?>')" class="btn btn-secondary d-flex align-items-center gap-1" onclick="return confirm('Are you sure you want to delete this?')"><i class="fa fa-trash"></i> Delete</a>
                                             <?php 

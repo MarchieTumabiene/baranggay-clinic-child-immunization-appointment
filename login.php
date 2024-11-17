4c2b33879535fae504cc2b67cb94bceaa98d7a27
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-11-18 06:00:58 +0800

    commit

--- login.php
+++ login.php
@@ -28,9 +28,9 @@
 </head>
 <body style="background: url('./assets/img/main-bnr.jpeg') no-repeat center/cover; ">
     
-    <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh; width: 500px; backdrop-filter: blur(3px);">
+    <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh;">
 
-        <div class="card rounded-0 shadow" style="width: 500px;">
+        <div class="card rounded-0 shadow" style="width: 500px; backdrop-filter: blur(3px);">
             <div class="card-body p-3">
                 <h3 class="text-center my-5">Barangay Child Immunization Appointment System</h3>
                 <form action="?login" method="post">

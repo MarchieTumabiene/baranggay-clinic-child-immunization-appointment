6999ae54fe2bdad88dbaf73a7b709d6a0e6a86d6
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-11-26 23:53:11 +0800

    commit

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -1,11 +1,11 @@
 <?php 
   require __DIR__ . '../../../function/config.php';
  
-  if ($_SERVER['REQUEST_URI'] != '/admin/index.php') {
-    if (!isset($_SESSION['ID']) && !isset($_SESSION['USERNAME'])) {
-      header('location: login.php');
-    }  
-  }
+  // if ($_SERVER['REQUEST_URI'] != '/admin/index.php') {
+  //   if (!isset($_SESSION['ID']) && !isset($_SESSION['USERNAME'])) {
+  //     header('location: login.php');
+  //   }  
+  // }
   
 $barangay = $_SESSION['BARANGAY'];
 $logo = $_SESSION['LOGO'];

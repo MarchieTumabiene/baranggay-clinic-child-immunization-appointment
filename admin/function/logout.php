7d26ec4c91530fd477118ca36e5a8521c9735e60
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-11-27 00:01:51 +0800

    commit

--- admin/function/logout.php
+++ admin/function/logout.php
@@ -2,5 +2,5 @@
 
     if ($_GET['action'] === 'logout') {
         session_destroy();
-        header('location: https://www.madridejosbarangayimmunization.com/index.php');
+        header('location: https://www.madridejosbarangayimmunization.com/');
     }

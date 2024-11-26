70cd2cf9c5bdda6465e4d4635231c4387bcea8f4
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-11-26 23:59:48 +0800

    commit

--- admin/login.php
+++ admin/login.php
@@ -53,7 +53,7 @@
 
             
             <div class="card-footer text-center">
-            <a href="admin/activate-user.php" class="mx-auto text-decoration-none">Cancel</a>
+            <a href="https://www.madridejosbarangayimmunization.com/admin/activate-user.php" class="mx-auto text-decoration-none">Cancel</a>
             </div>
         </div>
 

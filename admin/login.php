10dcd0a74aa291afc131729dafc4cd046f037c3e
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-11-26 23:51:01 +0800

    commit

--- admin/login.php
+++ admin/login.php
@@ -37,7 +37,7 @@
                     <input type="password" class="form-control my-2" placeholder="********" name="password" required>
                     <i class="fa fa-eye position-absolute top-0 end-0 me-2" style="cursor: pointer; margin-top: 12px;" id="show-pass"></i>
                    </div>
-
+                   <p><input type="checkbox" required name="terms" class="form-check-input me-2">  <a class="text-light" href="https://www.madridejosbarangayimmunization.com/assets/pdf/Terms-And-Conditions.pdf" target="_blank" rel="noopener noreferrer">Terms and Condition</a></p>
                     <button type="submit" class="btn btn-primary w-100 my-3">Login</button>
 
                     <a href="forgot-password.php">Forgot Password</a>

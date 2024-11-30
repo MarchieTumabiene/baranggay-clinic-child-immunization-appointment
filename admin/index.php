cbdcd41b0adcd6eb00c9eda7abcac1ba6ba1cdca
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-11-30 11:37:02 +0800

    commit

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,10 @@
 <?php 
 
-    $request = isset($_GET['page']) ? strtolower(clean($_GET['page'])) : 'home';
-    $auth = isset($_GET['auth']) ? strtolower(clean($_GET['auth'])) : 'home';
+    $request = isset($_GET['page']) ? strtolower(clean($_GET['page'])) : 'dashboard';
+    $auth = isset($_GET['auth']) ? strtolower(clean($_GET['auth'])) : 'dashboard';
     $viewPageUser = 'admin/';
 
-    if($request == 'dashboard'){
+    if($auth == 1 && $request == 'dashboard'){
         require __DIR__ . $viewPageUser . 'home.php';
     }
 

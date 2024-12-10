121ea38bb98524f5d114ac0893b4d1fd774c12d5
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-12-11 05:13:46 +0800

    commit

--- admin/index.php
+++ admin/index.php
@@ -1,10 +1,5 @@
 <?php
 require '../partials/headers.php';
-
-if($_SERVER['REQUEST_URI'] == '/admin/index'){
-  header("location: admin/");
-}
-
 require './partials/header.php';
 $today = date('Y-m-d');
 if($barangay != 'admin'){

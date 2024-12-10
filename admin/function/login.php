9569ce90e88247d04e70cfa3b5dcf826079f6bbb
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-12-11 05:30:21 +0800

    commit

--- admin/function/login.php
+++ admin/function/login.php
@@ -18,7 +18,7 @@
                 $_SESSION['BARANGAY'] = $row['barangay'];
                 $_SESSION['LOGO'] = $row['logo'];
                 unset($_SESSION['ADMIN_LOGIN']);
-                header('refresh:3;url=https://www.madridejosbarangayimmunization.com/admin/index.php');
+                header('refresh:3;url=https://www.madridejosbarangayimmunization.com/admin/dashboard.php');
             }else{
                 $error = "*Incorrect username or password";
             }

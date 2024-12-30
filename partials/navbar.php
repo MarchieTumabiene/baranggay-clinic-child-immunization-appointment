6a393b452791639b3f451d4c787c6d500d75c396
MarchieTumabiene <tumabienemarchie034@gmail.com>
2024-12-31 00:25:26 +0800

    commit

--- partials/navbar.php
+++ partials/navbar.php
@@ -9,26 +9,26 @@
 
         <button type="button" class="navbar-toggler " data-bs-toggle="collapse" data-bs-target="sidebar"><i class="fa fa-bars"></i></button>
 
-        <ul class="ms-auto navbar-nav gap-lg-3">
-
-        
-        <li class="nav-item">
-                <a href="?view=immunization" class="nav-link <?= isset($_GET['view']) ? $_GET['view'] == 'immunization' ? 'active' : ''  : 'active' ?>">Immunizaition</a>
-            </li>
+        <div class="navbar-collapse collapse">
+            <ul class="ms-auto navbar-nav gap-lg-3">
             <li class="nav-item">
-                <a href="?view=child" class="nav-link  <?= isset($_GET['view']) ? $_GET['view'] == 'child' ? 'active' : ''  : '' ?>">Child</a>
-            </li>
-            
-            <li class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle text-light" data-bs-toggle="dropdown"><i class="fa fa-gear"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end">
+                    <a href="?view=immunization" class="nav-link <?= isset($_GET['view']) ? $_GET['view'] == 'immunization' ? 'active' : ''  : 'active' ?>">Immunizaition</a>
+                </li>
+                <li class="nav-item">
+                    <a href="?view=child" class="nav-link  <?= isset($_GET['view']) ? $_GET['view'] == 'child' ? 'active' : ''  : '' ?>">Child</a>
+                </li>
+                
+                <li class="nav-item dropdown">
+                    <a href="#" class="nav-link dropdown-toggle text-light" data-bs-toggle="dropdown"><i class="fa fa-gear"></i></a>
+                    <ul class="dropdown-menu dropdown-menu-end">
 
-                    <li class="dropdown-item">
-                        <a href="#" onclick="showMessage('Are you sure you want to logout?', 'info', '?logout')" class="text-dark text-decoration-none"><i class="fa fa-sign-out"></i> Logout</a>
-                    </li>
-                </ul>
-            </li>
-        </ul>
+                        <li class="dropdown-item">
+                            <a href="#" onclick="showMessage('Are you sure you want to logout?', 'info', '?logout')" class="text-dark text-decoration-none"><i class="fa fa-sign-out"></i> Logout</a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
 

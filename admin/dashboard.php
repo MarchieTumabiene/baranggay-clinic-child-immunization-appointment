6180d0d5fcacbc915a06db1927fc5d897feef486
MarchieTumabiene <tumabienemarchie034@gmail.com>
2025-01-04 05:32:41 -0800

    commit

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -247,7 +247,7 @@ if ($get_child->num_rows > 0) {
             </div>
           </div>
 
-          <div class="col-12">
+          <div class="col-6">
             <div class="card">
               <div class="card-body">
               <canvas id="pieChart1" height="100px"></canvas>
@@ -255,6 +255,34 @@ if ($get_child->num_rows > 0) {
             </div>
           </div>
 
+          <script>
+            var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+            var yValues = [55, 49, 44, 24, 15];
+            var barColors = [
+              "#b91d47",
+              "#00aba9",
+              "#2b5797",
+              "#e8c3b9",
+              "#1e7145"
+            ];
+
+            new Chart("pieChart1", {
+              type: "pie",
+              data: {
+                labels: xValues,
+                datasets: [{
+                  backgroundColor: barColors,
+                  data: yValues
+                }]
+              },
+              options: {
+                title: {
+                  display: true,
+                  text: "World Wide Wine Production 2018"
+                }
+              }
+            });
+          </script>
 
         </div>
       </div>
@@ -281,43 +309,6 @@ if ($get_child->num_rows > 0) {
     $barangays[] = $value['barangay'];
   }
 ?>
-
-
-<script>
-            var xValues1 = ["japan"];
-            var yValues1 = [<?php echo json_encode($count); ?>];
-            var barColors1 = [
-              "#b91d47",
-              "#00aba9",
-              "#2b5797",
-              "#e8c3b9",
-              "#1e7145",
-              "#b91d47",
-              "#00aba9",
-              "#2b5797",
-              "#e8c3b9",
-              "#1e7145",
-              "#b91d47",
-              "#2b5797",
-            ];
-
-            new Chart("pieChart1", {
-              type: "pie",
-              data: {
-                labels: xValues1,
-                datasets: [{
-                  backgroundColor: barColors1,
-                  data: yValues1
-                }]
-              },
-              options: {
-                title: {
-                  display: true,
-                  text: "World Wide Wine Production 2018"
-                }
-              }
-            });
-          </script>
 
 
 
@@ -348,9 +339,6 @@ if ($get_child->num_rows > 0) {
     }
   });
 </script>
-
-
-
 <?php else: ?>
   <script>
   var xValues = ["Appointments", "Immunization Records", "Appointment Records"];

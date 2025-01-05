145085b8028bff4fe80f2df13667e9131bd10327
MarchieTumabiene <tumabienemarchie034@gmail.com>
2025-01-04 20:22:09 -0800

    commit

--- admin/function/logout.php
+++ admin/function/logout.php
@@ -2,5 +2,10 @@
 
     if ($_GET['action'] === 'logout') {
         session_destroy();
-        header('location: https://www.madridejosbarangayimmunization.com/');
+        $status = 1;
+        $updateStatus = $conn->prepare("UPDATE admin SET status = ? WHERE id = ?");
+        $updateStatus->bind_param("ii", $status, $_SESSION['ID']);
+        if($updateStatus->execute()){
+            header('location: https://www.madridejosbarangayimmunization.com/');
+        }
     }
